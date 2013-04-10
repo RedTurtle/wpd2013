537728b1ab089dd72373acd8a9dbbbba0a4c03f6
Andrew Mleczko <andrew@mleczko.net>
2013-04-10 17:59:27 +0200

    new scripts

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,7 @@
 from fabric.api import sudo, cd, env
 from fabric.utils import abort
 from fabric.colors import green
+from fabric.contrib.files import exists
 
 def staging():
     """Use staging server"""
@@ -18,9 +19,22 @@ def staging():
     env.hosts = [env['wpd2013.staging.server']]
     env.user = env['wpd2013.staging.user']
 
+def freshdb():
+    """If Data.fs_fresh is available it will be used as a new ZODB"""
+    if exists('%s/var/filestorage/Data.fs_fresh' % env.directory, use_sudo=True):
+        with cd(env.directory):
+            sudo("./bin/instance stop")
+        with cd('%s/var/filestorage' % env.directory):
+            sudo("mv Data.fs Data.fs.old")
+            sudo("cp Data.fs_fresh Data.fs")
+            sudo("chown plone: ./ -R")
+        with cd(env.directory):
+            sudo("./bin/instance start")
+
 def buildout():
     """Pull buildout, run bin/buildount, touch wsgi"""
     with cd(env.directory):
         sudo("git pull")
+        sudo("./bin/instance stop")
         sudo("./bin/buildout -NU")
-        sudo("./bin/instance restart")
+        sudo("./bin/instance start")
